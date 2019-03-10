6718550689a68ff8bd32125be53d37fc0364401c
defunctec <defuncteconomics@gmail.com>
2019-03-10 16:21:51 +0000

    Version Change
    Revert to latest mainnet client

--- crownpiscript.sh
+++ crownpiscript.sh
@@ -27,7 +27,7 @@ download_package() {
         mkdir -p $dir
     fi
     # Change this later to take latest release version.
-    sudo wget "https://gitlab.crown.tech/crown/crown-core/-/jobs/5409/artifacts/download" -O $dir/crown.zip
+    sudo wget "https://gitlab.crown.tech/crown/crown-core/-/jobs/4986/artifacts/download" -O $dir/crown.zip
 }
 
 # Install Crown client
