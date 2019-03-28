ea1d6fbe7e5ae72bca423425d0e074180c9749f7
defunctec <defuncteconomics@gmail.com>
2019-03-28 12:27:50 +0000

    Update to 0.13.0
    Jade release

--- crownpiscript.sh
+++ crownpiscript.sh
@@ -18,7 +18,7 @@ update_repos() {
 # Download Crown client (Update link with new client)
 download_package() {
     # Password change prompt
-    echo Getting 0.12.7.3
+    echo Getting 0.13.0 MN-PoS client
     # Create temporary directory
     dir=`mktemp -d`
     if [ -z "$dir" ]; then
@@ -27,7 +27,7 @@ download_package() {
         mkdir -p $dir
     fi
     # Change this later to take latest release version.
-    sudo wget "https://github.com/Crowndev/crown-core/releases/download/v0.12.5.3/Crown-0.12.5.3-RaspberryPi.zip" -O $dir/crown.zip
+    sudo wget "https://github.com/Crowndev/crown-core/releases/download/v0.13.0/Crown-0.13.0.0-RaspberryPi.zip" -O $dir/crown.zip
 }
 
 # Install Crown client
